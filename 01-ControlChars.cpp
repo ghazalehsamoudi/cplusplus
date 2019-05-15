dbb7b213456b686865871bf3f5d4b80c48b667a2
Omid Lak Mazaheri <omidlmazaheri@gmail.com>
2019-05-15 22:57:44 +0430

    Add files via upload

---
+++ 01-ControlChars.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+main()
+{
+	//tab:4 chars
+	//control cahrs
+	//n-endl
+	//cout<<"hello"<<' '<<"world!";
+	//cout<<"hello"<<'\t'<<"world!";
+	//cout<<"hello"<<'\n'<<"world!";
+	//cout<<"hello"<<endl<<"world!";
+	//cout<<"hello"<<'\b'<<"world!";
+	//cout<<"hello"<<'\b';
+	cout<<"hello"<<'\a';
+	getch();
+}
